116d4623700ddffc514a0db1a62d8e323bf7ed34
minhphai <minhphai87@gmail.com>
2019-02-15 17:12:25 +0700

    Update __init__.py

--- torchreid/datasets/__init__.py
+++ torchreid/datasets/__init__.py
@@ -17,7 +17,7 @@ from .mars import Mars
 from .ilidsvid import iLIDSVID
 from .prid2011 import PRID2011
 from .dukemtmcvidreid import DukeMTMCVidReID
-
+from .grozi import GROZI
 
 __imgreid_factory = {
     'market1501': Market1501,
@@ -30,6 +30,7 @@ __imgreid_factory = {
     'prid450s': PRID450S,
     'ilids': iLIDS,
     'sensereid': SenseReID,
+    'grozi':GROZI
 }
 
 
@@ -43,11 +44,11 @@ __vidreid_factory = {
 
 def init_imgreid_dataset(name, **kwargs):
     if name not in list(__imgreid_factory.keys()):
-        raise KeyError('Invalid dataset, got "{}", but expected to be one of {}'.format(name, list(__imgreid_factory.keys())))
+        raise KeyError("Invalid dataset, got '{}', but expected to be one of {}".format(name, list(__imgreid_factory.keys())))
     return __imgreid_factory[name](**kwargs)
 
 
 def init_vidreid_dataset(name, **kwargs):
     if name not in list(__vidreid_factory.keys()):
-        raise KeyError('Invalid dataset, got "{}", but expected to be one of {}'.format(name, list(__vidreid_factory.keys())))
+        raise KeyError("Invalid dataset, got '{}', but expected to be one of {}".format(name, list(__vidreid_factory.keys())))
     return __vidreid_factory[name](**kwargs)
